cf92cc5b6ad05a0b9b70e93ae7b4ac6e5e83abe3
merhawi <merhawi.temnewo88@gmail.com>
2019-06-08 11:30:09 -0500

    beautify

--- src/components/RoomList/index.js
+++ src/components/RoomList/index.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
 import Room from '../Room/Room';
-import './tourlist.scss';
+import './roomlist.scss';
 import { roomData } from '../../roomData';
 
 export default class RoomList extends Component {
   state = {
     rooms: roomData
   };
+  handleClick = () => {};
   render() {
     const { rooms } = this.state;
     return (
       <section className="tourlist">
         <div className="ui four cards">
-          {rooms.map(room => (
-            <a className="blue card">
-              <Room key={room.id} details={room} />
-            </a>
-          ))}
+          {rooms.map(room =>
+            room.active === true ? (
+              <a className="blue card">
+                <Room
+                  key={room.id}
+                  details={room}
+                  onClick={this.handleClick(room.id)}
+                />
+              </a>
+            ) : (
+              <a className="white card">
+                <Room key={room.id} details={room} />
+              </a>
+            )
+          )}
         </div>
       </section>
     );
